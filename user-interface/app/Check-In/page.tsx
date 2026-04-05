b6131381f56b62e503bad6df17a7ece31a695775
Elliot Huffman <elliot@huffman.me>
2026-04-05 18:34:59 -0400

    Create Place Holders
    Initial place holders for the functionality.

---
+++ user-interface/app/Check-In/page.tsx
@@ -0,0 +1,11 @@
+/**
+ * Check in page.
+ * @returns Rendered check in page.
+ */
+export default function Page(): React.ReactNode {
+    return (
+        <>
+            Hello
+        </>
+    );
+}
---
+++ user-interface/app/MemberManagement/page.tsx
@@ -0,0 +1,11 @@
+/**
+ * Initial entry point for the member management section of the application.
+ * @returns Rendered member management landing page.
+ */
+export default function Page(): React.ReactNode {
+    return (
+        <>
+            Coming soon!
+        </>
+    );
+}
